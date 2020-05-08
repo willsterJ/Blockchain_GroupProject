de990c6c1060f7350d9556853f17e3cfa07260a7
Julian Smith De Niro <jsd410@nyu.edu>
2020-05-07 20:37:49 -0400

    started account balance. working on iterating through database. started last 4 functions.

--- dev_mode_cli_run.sh
+++ dev_mode_cli_run.sh
@@ -1,86 +1,35 @@
 # create the identitites for the orgs
+chmod 777 /opt/trade/createIdentity.sh
 /opt/trade/createIdentity.sh
 
 # install and instantiate chaincode
 peer chaincode install -p chaincodedev/chaincode/trade_workflow_v1 -n tw -v 0
-peer chaincode instantiate -n tw -v 0 -c '{"Args":["init","LumberInc","LumberBank","100000","WoodenToys","ToyBank","200000","UniversalFreight","ForestryDepartment","Lender","5000000"]}' -C tradechannel
+peer chaincode instantiate -n tw -v 0 -c '{"Args":["init","Buyer", "100000","Seller","200000","Middleman","200000", "Warehouse","50000", "Carrier", "10000"]}' -C tradechannel
 sleep 2
 
 #importer requests trade from exporter
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/importer
-peer chaincode invoke -n tw -c '{"Args":["requestTrade", "trade-1", "5000", "Wood for Toys"]}' -C tradechannel
+export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/seller
+peer chaincode invoke -n tw -c '{"Args":["initItem", "laptop", "1200", "20"]}' -C tradechannel
+sleep 2
+peer chaincode invoke -n tw -c '{"Args":["queryItems", "{\"selector\":{\"descriptionOfGoods\":\"laptop\"}}"]}' -C tradechannel
+sleep 2
+peer chaincode invoke -n tw -c '{"Args":["updateItem", "sellerlaptop", "2"]}' -C tradechannel
 sleep 2
 
-#exporter accepts trade
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/exporter
-peer chaincode invoke -n tw -c '{"Args":["acceptTrade", "trade-1"]}' -C tradechannel
+export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/buyer
+peer chaincode invoke -n tw -c '{"Args":["requestTrade", "trade-1", "1", "laptop"]}' -C tradechannel
 sleep 2
-
-#importer requests LC
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/importer
-peer chaincode invoke -n tw -c '{"Args":["requestLC", "trade-1"]}' -C tradechannel
+peer chaincode invoke -n tw -c '{"Args": ["acceptTrade", "trade-1"]}' -C tradechannel
 sleep 2
-
-#importer issues LC
-peer chaincode invoke -n tw -c '{"Args":["issueLC", "trade-1", "LC-1", "2020"]}' -C tradechannel
-sleep 2
-
-#exporter accepts LC
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/exporter
-peer chaincode invoke -n tw -c '{"Args":["acceptLC", "trade-1"]}' -C tradechannel
-sleep 2
-
-#exporter requests EL
-peer chaincode invoke -n tw -c '{"Args":["requestEL", "trade-1"]}' -C tradechannel
-sleep 2
-
-#regulatory authority issues EL
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/regulator
-peer chaincode invoke -n tw -c '{"Args":["issueEL", "trade-1", "LC-1", "2021"]}' -C tradechannel
-sleep 2
-
-#exporter prepares shipment
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/exporter
-peer chaincode invoke -n tw -c '{"Args":["prepareShipment", "trade-1"]}' -C tradechannel
-sleep 2
-
-#carrier accepts shipment and bill of lading
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/carrier
-peer chaincode invoke -n tw -c '{"Args":["acceptShipmentAndIssueBL", "trade-1","BL-1","2020","new york", "new jersey"]}' -C tradechannel
-sleep 2
-
-#exporter requests payment from lender
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/exporter
 peer chaincode invoke -n tw -c '{"Args":["requestPayment", "trade-1"]}' -C tradechannel
 sleep 2
-
-#lender requests accepted LC from exporter
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/lender
-peer chaincode invoke -n tw -c '{"Args":["requestAcceptedLC", "trade-1"]}' -C tradechannel
+peer chaincode invoke -n tw -c '{"Args":["makePayment", "trade-1"]}' -C tradechannel
 sleep 2
-
-#lender issues accepted LC to exporter
-peer chaincode invoke -n tw -c '{"Args":["issueAcceptedLC", "trade-1","AcceptedLC-1","2020"]}' -C tradechannel
+peer chaincode invoke -n tw -c '{"Args":["getAccountBalance", "trade-1", "seller"]}' -C tradechannel
+sleep 2""
+peer chaincode invoke -n tw -c '{"Args":["getAccountBalance", "trade-1", "middleman"]}' -C tradechannel
 sleep 2
-
-#exporter accepts accepted LC from lender
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/exporter
-peer chaincode invoke -n tw -c '{"Args":["acceptAcceptedLC", "trade-1"]}' -C tradechannel
+peer chaincode invoke -n tw -c '{"Args":["getAccountBalance", "trade-1", "carrier"]}' -C tradechannel
 sleep 2
-
-#lender makes payment to exporter
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/lender
-peer chaincode invoke -n tw -c '{"Args":["makePayment", "trade-1", "exporter"]}' -C tradechannel
-peer chaincode invoke -n tw -c '{"Args":["getAccountBalance", "trade-1", "lender"]}' -C tradechannel
-peer chaincode invoke -n tw -c '{"Args":["getAccountBalance", "trade-1", "exporter"]}' -C tradechannel
-sleep 2
-
-#lender requests payment from importer in 60 days
-peer chaincode invoke -n tw -c '{"Args":["requestPayment", "trade-1"]}' -C tradechannel
-sleep 2
-
-#importer makes payment to lender
-export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/importer
-peer chaincode invoke -n tw -c '{"Args":["makePayment", "trade-1", "lender"]}' -C tradechannel
-peer chaincode invoke -n tw -c '{"Args":["getAccountBalance", "trade-1", "importer"]}' -C tradechannel
-peer chaincode invoke -n tw -c '{"Args":["getAccountBalance", "trade-1", "lender"]}' -C tradechannel
+peer chaincode invoke -n tw -c '{"Args":["getAccountBalance", "trade-1", "warehouse"]}' -C tradechannel
+sleep 5
--- dev_mode_test_run.sh
+++ dev_mode_test_run.sh
@@ -1,10 +1,10 @@
 # create the identitites for the orgs
-chmod 777 -r /opt/trade/createIdentity.sh
+chmod 777 /opt/trade/createIdentity.sh
 /opt/trade/createIdentity.sh
 
 # install and instantiate chaincode
 peer chaincode install -p chaincodedev/chaincode/trade_workflow_v1 -n tw -v 0
-peer chaincode instantiate -n tw -v 0 -c '{"Args":["init","Buyer", "100000","Seller","200000","Middleman","200000", "Warehouse","50000", "Carrier"]}' -C tradechannel
+peer chaincode instantiate -n tw -v 0 -c '{"Args":["init","Buyer", "100000","Seller","200000","Middleman","200000", "Warehouse","50000", "Carrier", "10000"]}' -C tradechannel
 sleep 2
 
 #importer requests trade from exporter
@@ -14,5 +14,10 @@ sleep 2
 peer chaincode invoke -n tw -c '{"Args":["queryItems", "{\"selector\":{\"descriptionOfGoods\":\"laptop\"}}"]}' -C tradechannel
 sleep 2
 peer chaincode invoke -n tw -c '{"Args":["updateItem", "sellerlaptop", "2"]}' -C tradechannel
+sleep 2
 
-
+export CORE_PEER_MSPCONFIGPATH=/root/.fabric-ca-client/buyer
+peer chaincode invoke -n tw -c '{"Args":["requestTrade", "trade-1", "1", "laptop"]}' -C tradechannel
+sleep 2
+peer chaincode invoke -n tw -c '{"Args": ["acceptTrade", "trade-1"]}' -C tradechannel
+sleep 2
