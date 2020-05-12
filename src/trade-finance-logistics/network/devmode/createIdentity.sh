7d8c9c6c663b8e785bb27d9bfe83a4be212082d9
Julian Smith De Niro <jsd410@nyu.edu>
2020-05-12 11:14:20 -0400

    added two new orgs to dev-mode

--- src/trade-finance-logistics/network/devmode/createIdentity.sh
+++ src/trade-finance-logistics/network/devmode/createIdentity.sh
@@ -1,6 +1,6 @@
 set -e
 
-ORG_NAME="buyer"
+ORG_NAME="buyer0"
 
 fabric-ca-client enroll -u http://admin:adminpw@ca:7054 --mspdir admin
 
@@ -17,13 +17,30 @@ ORG_NAME="admin"
 mkdir ~/.fabric-ca-client/${ORG_NAME}/admincerts
 cp -p ~/.fabric-ca-client/${ORG_NAME}/signcerts/* ~/.fabric-ca-client/${ORG_NAME}/admincerts
 
-ORG_NAME="seller"
+ORG_NAME="buyer1"
+fabric-ca-client register --id.name ${ORG_NAME} --id.secret pwd1 --id.type user \
+    --id.attrs "role=buyer" -u http://ca:7054
+fabric-ca-client enroll -u http://${ORG_NAME}:pwd1@ca:7054 \
+    --enrollment.attrs "role,email:opt" --mspdir ${ORG_NAME}
+mkdir ~/.fabric-ca-client/${ORG_NAME}/admincerts
+cp -p ~/.fabric-ca-client/${ORG_NAME}/signcerts/* ~/.fabric-ca-client/${ORG_NAME}/admincerts
+
+ORG_NAME="seller0"
 fabric-ca-client register --id.name ${ORG_NAME} --id.secret pwd1 --id.type user \
     --id.attrs "role=seller" -u http://ca:7054
 fabric-ca-client enroll -u http://${ORG_NAME}:pwd1@ca:7054 \
     --enrollment.attrs "role,email:opt" --mspdir ${ORG_NAME}
 mkdir ~/.fabric-ca-client/${ORG_NAME}/admincerts
 cp -p ~/.fabric-ca-client/${ORG_NAME}/signcerts/* ~/.fabric-ca-client/${ORG_NAME}/admincerts
+
+ORG_NAME="seller1"
+fabric-ca-client register --id.name ${ORG_NAME} --id.secret pwd1 --id.type user \
+    --id.attrs "role=seller" -u http://ca:7054
+fabric-ca-client enroll -u http://${ORG_NAME}:pwd1@ca:7054 \
+    --enrollment.attrs "role,email:opt" --mspdir ${ORG_NAME}
+mkdir ~/.fabric-ca-client/${ORG_NAME}/admincerts
+cp -p ~/.fabric-ca-client/${ORG_NAME}/signcerts/* ~/.fabric-ca-client/${ORG_NAME}/admincerts
+
 
 ORG_NAME="middleman"
 fabric-ca-client register --id.name ${ORG_NAME} --id.secret pwd1 --id.type user \
